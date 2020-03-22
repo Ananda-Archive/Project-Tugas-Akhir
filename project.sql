835df7efcb123c258f4b369d76122b3c670dcdff
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-03-22 11:18:37 +0700

    sql update

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2020 at 05:57 AM
+-- Generation Time: Mar 22, 2020 at 05:17 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,23 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `time`
+--
+
+CREATE TABLE `time` (
+  `time_start` timestamp NOT NULL DEFAULT current_timestamp(),
+  `time_end` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `nim` text NOT NULL,
+  `nomor` varchar(20) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `profile_picture` varchar(100) NOT NULL
+  `profile_picture` varchar(100) NOT NULL,
+  `role` tinyint(4) NOT NULL DEFAULT 0,
+  `dosen_pembimbing` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `nim`, `nama`, `password`, `profile_picture`) VALUES
-(1, '24060117130048', 'Ananda Prabu Tritya Vijaya', '123456', 'src/img/profile/24060117130048.jpg');
+INSERT INTO `user` (`id`, `nomor`, `nama`, `password`, `profile_picture`, `role`, `dosen_pembimbing`) VALUES
+(1, '24060117130048', 'Ananda Prabu Tritya Vijaya', '123456', 'src/img/profile/24060117130048.jpg', 2, NULL);
 
 --
 -- Indexes for dumped tables
@@ -51,7 +64,8 @@ INSERT INTO `user` (`id`, `nim`, `nama`, `password`, `profile_picture`) VALUES
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dosen_pembimbing` (`dosen_pembimbing`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -62,6 +76,16 @@ ALTER TABLE `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`dosen_pembimbing`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
