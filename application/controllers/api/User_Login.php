03343747b54a0c04bbad7bec22aee02f06cc04fe
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-03-22 11:25:07 +0700

    add manual security for API access

--- application/controllers/api/User_Login.php
+++ application/controllers/api/User_Login.php
@@ -21,70 +21,79 @@ class User_Login extends REST_Controller {
 
 
     public function index_post(){
-        $nama = $this->post('nama');
-        $nomor = $this->post('nomor');
-        $password = $this->post('password');
-        // Kalau parameter nomor kosong
-        if(!isset($nomor)) {
-            $this->response(
-                array(
-                    'status' => FALSE,
-                    'message' => $this::REQUIRED_PARAMETER_MESSAGE."nomor"
-                ), REST_Controller::HTTP_BAD_REQUEST
-            );
-            return;
-        }
-        // kalau parameter password kosong
-        if(!isset($password)) {
-            $this->response(
-                array(
-                    'status' => FALSE,
-                    'message' => $this::REQUIRED_PARAMETER_MESSAGE."password"
-                ), REST_Controller::HTTP_BAD_REQUEST
-            );
-            return;
-        }
-        // Kalau parameter terisi dan benar
-        if($this->M_User->login($nomor,$password)->num_rows() > 0) {
-            $data = $this->M_User->login($nomor,$password)->row_array();
-            $data_session = array(
-                'id' => $data['id'],
-                'nama' => $data['nama'],
-                'nomor' => $data['nomor'],
-                'role' => $data['role']
-            );
-            $this->session->set_userdata($data_session);
-            $this->response(
-                array(
-                    'status' => TRUE,
-                    'message' => $this::LOGIN_SUCCESS_MESSAGE
-                ), REST_Controller::HTTP_CREATED
-            );
-        } else {
-            if($this->M_User->get_by_nomor($nomor)->num_rows() > 0) {
+        if(!$this->session->userdata('id')) {
+            $nama = $this->post('nama');
+            $nomor = $this->post('nomor');
+            $password = $this->post('password');
+            // Kalau parameter nomor kosong
+            if(!isset($nomor)) {
                 $this->response(
                     array(
                         'status' => FALSE,
-                        'message' => $this::INCORRECT_PASSWORD_MESSAGE
-                    ), REST_Controller::HTTP_UNAUTHORIZED
+                        'message' => $this::REQUIRED_PARAMETER_MESSAGE."nomor"
+                    ), REST_Controller::HTTP_BAD_REQUEST
+                );
+                return;
+            }
+            // kalau parameter password kosong
+            if(!isset($password)) {
+                $this->response(
+                    array(
+                        'status' => FALSE,
+                        'message' => $this::REQUIRED_PARAMETER_MESSAGE."password"
+                    ), REST_Controller::HTTP_BAD_REQUEST
+                );
+                return;
+            }
+            // Kalau parameter terisi dan benar
+            if($this->M_User->login($nomor,$password)->num_rows() > 0) {
+                $data = $this->M_User->login($nomor,$password)->row_array();
+                $data_session = array(
+                    'id' => $data['id'],
+                    'nama' => $data['nama'],
+                    'nomor' => $data['nomor'],
+                    'role' => $data['role']
+                );
+                $this->session->set_userdata($data_session);
+                $this->response(
+                    array(
+                        'status' => TRUE,
+                        'message' => $this::LOGIN_SUCCESS_MESSAGE
+                    ), REST_Controller::HTTP_CREATED
                 );
             } else {
-                if($this->M_User->get_by_nomor($nomor)->num_rows() == 0) {
+                if($this->M_User->get_by_nomor($nomor)->num_rows() > 0) {
                     $this->response(
                         array(
                             'status' => FALSE,
-                            'message' => $this::USER_NOT_FOUND_MESSAGE
+                            'message' => $this::INCORRECT_PASSWORD_MESSAGE
                         ), REST_Controller::HTTP_UNAUTHORIZED
                     );
                 } else {
-                    $this->response(
-                        array(
-                            'status' => FALSE,
-                            'message' => $this::LOGIN_FAILED_MESSAGE
-                        ), REST_Controller::HTTP_INTERNAL_SERVER_ERROR
-                    );
+                    if($this->M_User->get_by_nomor($nomor)->num_rows() == 0) {
+                        $this->response(
+                            array(
+                                'status' => FALSE,
+                                'message' => $this::USER_NOT_FOUND_MESSAGE
+                            ), REST_Controller::HTTP_UNAUTHORIZED
+                        );
+                    } else {
+                        $this->response(
+                            array(
+                                'status' => FALSE,
+                                'message' => $this::LOGIN_FAILED_MESSAGE
+                            ), REST_Controller::HTTP_INTERNAL_SERVER_ERROR
+                        );
+                    }
                 }
             }
+        } else {
+            $this->response(
+                array(
+                    'status' => FALSE,
+                    'message' => $this::LOGIN_FAILED_MESSAGE
+                ), REST_Controller::HTTP_INTERNAL_SERVER_ERROR
+            );
         }
     }
 }
