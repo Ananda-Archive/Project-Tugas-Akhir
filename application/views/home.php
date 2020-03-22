3e2fa429c0b9f1c0d158a84563810e7338f9a31c
Ananda Prabu Tritya Vijaya <49684759+anandavj@users.noreply.github.com>
2020-03-22 11:16:11 +0700

    manage home menu

--- application/views/home.php
+++ application/views/home.php
@@ -9,35 +9,6 @@
 					<v-layout fill-height>
                         <v-container fluid>
                             <v-row :align="center">
-                                <v-col cols="12" sm="12" md="4">
-                                    <v-card @click="" class="elevation-12 align-center" color="blue" min-height="245" max-height="245">
-										<div class="d-flex flex-no-wrap justify-space-between">
-											<div>
-												<v-card-title class="font-weight-light">STATUS PENDAFTARAN</v-card-title>
-											</div>
-											<div>
-												<v-card-title class="font-weight-light"><v-icon>mdi-chart-timeline-variant</v-icon></v-card-title>
-											</div>
-										</div>
-                                        <v-card-title class="red--text justify-center display-4 mb-n4">0</v-card-title>
-                                        <v-card-title class="justify-center font-weight-thin body-1">Mahasiswa Belum Terdaftar</v-card-title>
-                                    </v-card>
-                                </v-col>
-								<v-col cols="12" sm="12" md="4">
-                                    <v-card @click="" class="elevation-12 align-center" color="blue" min-height="245" max-height="245">
-                                        <div class="d-flex flex-no-wrap justify-space-between">
-											<div>
-												<v-card-title class="font-weight-light">JADWAL PENDAFTARAN</v-card-title>
-											</div>
-											<div>
-												<v-card-title class="font-weight-light"><v-icon>mdi-calendar-clock</v-icon></v-card-title>
-											</div>
-										</div>
-                                        <v-card-title class="justify-center font-weight-thin headline">12 Januari 2020</v-card-title>
-                                        <v-card-title class="justify-center caption ma-n4">s/d</v-card-title>
-                                        <v-card-title class="justify-center font-weight-thin headline">30 Maret 2020</v-card-title>
-                                    </v-card>
-                                </v-col>
 								<v-col cols="12" sm="12" md="4">
                                     <v-card @click="" class="elevation-12 align-center" color="blue" min-height="245" max-height="245">
                                         <div class="d-flex flex-no-wrap justify-space-between">
