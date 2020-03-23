38255b3c390a4eda5c99c92a55a1ab4fb3be83a5
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-03-23 07:57:51 +0700

    add lodash and moment.js

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,6 +9,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
         <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+		<!-- Vuetify -->
 		<script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+		<!-- Axios -->
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-		<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+		<!-- Lodash -->
+		<script src="https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js"></script>
+		<!-- moment.js -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
